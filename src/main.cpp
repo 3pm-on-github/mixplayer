67a5b563689c48fe63292855e05e1f0fe10eaf4d
3pm-on-github <pookiemon2019@gmail.com>
2025-11-22 14:53:06 +0100

    ok garmin fix this

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include <QTransform>
 #include <QMouseEvent>
 #include <QtMath>
+#include <QDebug>
 #include "DiscLabel.h"
 #include "main.moc"
 
@@ -24,10 +25,14 @@ int main(int argc, char *argv[]) {
 
     QVBoxLayout *layout = new QVBoxLayout;
 
-    QMediaPlayer *player = new QMediaPlayer;
-    QAudioOutput *audioOutput = new QAudioOutput;
+    QMediaPlayer *player = new QMediaPlayer(&window);
+    QMediaPlayer *sfxplayer = new QMediaPlayer(&window);
+    QAudioOutput *audioOutput = new QAudioOutput(&window);
+    QAudioOutput *sfxAudioOutput = new QAudioOutput(&window);
     player->setAudioOutput(audioOutput);
+    sfxplayer->setAudioOutput(sfxAudioOutput);
     audioOutput->setVolume(0.8);
+    sfxAudioOutput->setVolume(0.9);
 
     DiscLabel *discLabel = new DiscLabel;
     QPixmap discPixmap(":/disc.png");
@@ -80,20 +85,74 @@ int main(int argc, char *argv[]) {
 
     QPushButton *playButton = new QPushButton("Play");
     QPushButton *stopButton = new QPushButton("Stop");
+    QPushButton *sfx1Button = new QPushButton("SFX 1");
+    QPushButton *sfx2Button = new QPushButton("SFX 2");
+    QPushButton *sfx3Button = new QPushButton("SFX 3");
+    QPushButton *sfx4Button = new QPushButton("SFX 4");
     QPushButton *openButton = new QPushButton("Open Music");
     layout->addWidget(playButton);
     layout->addWidget(stopButton);
+    layout->addWidget(sfx1Button);
+    layout->addWidget(sfx2Button);
+    layout->addWidget(sfx3Button);
+    layout->addWidget(sfx4Button);
     layout->addWidget(openButton);
+    playButton->setEnabled(false);
+    stopButton->setEnabled(false);
 
-    QObject::connect(playButton, &QPushButton::clicked, [=]() { player->play(); timer->start(50); });
-    QObject::connect(stopButton, &QPushButton::clicked, [=]() { player->stop(); timer->stop(); });
+    QObject::connect(playButton, &QPushButton::clicked, [=]() mutable {
+        if(player->playbackState() == QMediaPlayer::PlayingState) {
+            player->pause();
+            timer->stop();
+            playButton->setText("Play");
+        } else {
+            player->play();
+            timer->start(50);
+            playButton->setText("Pause");
+        }
+    });
+
+    QObject::connect(stopButton, &QPushButton::clicked, [=]() { player->stop(); timer->stop(); playButton->setText("Play"); });
+    QObject::connect(sfx1Button, &QPushButton::clicked, [=]() {
+        sfxplayer->setSource(QUrl::fromLocalFile("sfx1.wav"));
+        sfxplayer->play();
+    });
+    QObject::connect(sfx2Button, &QPushButton::clicked, [=]() {
+        sfxplayer->setSource(QUrl::fromLocalFile("sfx2.wav"));
+        sfxplayer->play();
+    });
+    QObject::connect(sfx3Button, &QPushButton::clicked, [=]() {
+        sfxplayer->setSource(QUrl::fromLocalFile("sfx3.wav"));
+        sfxplayer->play();
+    });
+    QObject::connect(sfx4Button, &QPushButton::clicked, [=]() {
+        sfxplayer->setSource(QUrl::fromLocalFile("sfx4.wav"));
+        sfxplayer->play();
+    });
     QObject::connect(openButton, &QPushButton::clicked, [&]() {
         QString file = QFileDialog::getOpenFileName(&window,"Select Music","","Audio Files (*.mp3 *.wav *.flac)");
         if(!file.isEmpty()) {
             player->setSource(QUrl::fromLocalFile(file));
             QFileInfo info(file);
             openButton->setText(info.fileName());
+            playButton->setEnabled(true);
+            stopButton->setEnabled(true);
+            playButton->setText("Play");
         }
+    });
+
+    QObject::connect(player, &QMediaPlayer::errorOccurred, [&](QMediaPlayer::Error err, const QString &msg){
+        Q_UNUSED(err)
+        qDebug() << "player error:" << msg;
+    });
+    QObject::connect(sfxplayer, &QMediaPlayer::errorOccurred, [&](QMediaPlayer::Error err, const QString &msg){
+        Q_UNUSED(err)
+        qDebug() << "sfxplayer error:" << msg;
+    });
+
+    QObject::connect(player, &QMediaPlayer::playbackStateChanged, [&](QMediaPlayer::PlaybackState state){
+        if(state == QMediaPlayer::PlayingState) playButton->setText("Pause");
+        else playButton->setText("Play");
     });
 
     window.setLayout(layout);
